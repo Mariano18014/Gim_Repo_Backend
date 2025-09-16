179aebff6cf9db3bb44f14b4fe7f4d9a2fb7f621
Mariano18014 <hasanmariano5@gmail.com>
2025-09-16 17:36:11 -0300

    Primer commit, creación del proyecto.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express.app();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//app.use('/route/user', new UserRoute().start());
+//app.use('', );
+
+const PORT = process.env.PORT;
+
+const server = app.listen(PORT, () => console.log(`Server listening on. http://localhost:${PORT}`));
+
+server.on('error', error  => console.log(`Error in server, ${error.message}`));
